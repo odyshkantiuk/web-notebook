88493f73d2ce606114159ea341ad947eb22495f1
odyshkantiuk <odyshkantiuk@gmail.com>
2023-10-13 15:56:20 +0300

    Initial commit

---
+++ src/main/java/com/webnote/webnotebook/WebNotebookApplication.java
@@ -0,0 +1,13 @@
+package com.webnote.webnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNotebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webnote/webnotebook/WebNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webnote.webnotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
